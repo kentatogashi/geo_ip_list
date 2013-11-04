b85efa26adb59a872f0c125bbbbe4d0a93e40339
kenta_togasi@yahoo.co.jp <kenta_togasi@yahoo.co.jp>
2013-11-04 21:50:47 +0900

    small modifing class loader

--- lib/ClassLoader.php
+++ lib/ClassLoader.php
@@ -1,16 +1,26 @@
 <?php
 class ClassLoader {
 
+    private $base;
+
     public function __construct() {
 	spl_autoload_register(array($this,'loadClass'));
     }
 
-    public static function loadClass($class) {
-	$file_name = $class . ".php";
+    public function loadClass($class) {
+	$file_name = $this->base . $class . ".php";
 	if(file_exists($file_name)) { 
 	    include $file_name;
+	    return true;
 	}
+	die("load error");	
     }
+
+    public function setBaseDir($base) {
+	$this->base = $base;
+    }
+
 }
 
-new ClassLoader();
+$loader = new ClassLoader();
+$loader->setBaseDir("lib/");
